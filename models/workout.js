b958f421fabe135ac8a345533445301a0b335de7
Crystal <crystalwatkins428@gmail.com>
2020-07-27 18:57:05 -0400

    updated

--- models/workout.js
+++ models/workout.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const opts = {toJSON: {virtuals:true} };
+const opts = {toJSON: { virtuals: true } };
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
