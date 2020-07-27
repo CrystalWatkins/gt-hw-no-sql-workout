ab927f004a01193f135d53206fcfb9acdbb93a9c
Crystal <crystalwatkins428@gmail.com>
2020-07-26 22:23:21 -0400

    apiroutes still need work

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,90 +1,79 @@
-// const express = require("express");
-// const router = express.Router();
-// const db = require("../models");
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
 
-// router.get("/api/pizzas", (req, res) => {
-//   db.Pizza.find({})
-//     // .populate("ingredients")
-//     .populate({
-//       path: "ingredients", // populate with user collection
-//       options: {
-//         limit: null, // query string or null
-//         skip: null, // query string or null
-//       },
-//     })
-//     .then((foundPizzas) => {
-//       res.json({
-//         error: false,
-//         data: foundPizzas,
-//         message: "All pizzas retrieved.",
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({
-//         error: true,
-//         data: null,
-//         message: "Unable to retrieve all pizzas.",
-//       });
-//     });
-// });
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((dbWorkouts) => {
+        res.json({
+            error: false,
+            data: foundWorkouts,
+            message: "All Workouts retrieved.",
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to retrieve all workouts.",
+      });
+    });
+});
 
-// router.post("/api/pizzas", (req, res) => {
-//   // Sanitize req.body inputs
-//   if (
-//     !req.body.name ||
-//     !req.body.name.trim().length ||
-//     !req.body.price ||
-//     !req.body.price.trim().length
-//   ) {
-//     return res.status(400).json({
-//       error: true,
-//       data: null,
-//       message: "Please enter valid information.",
-//     });
-//   }
+router.post("/api/workouts", (req, res) => {
+  // Sanitize req.body inputs
+  if (
+    !req.body.name ||
+    !req.body.name.trim().length
+  ) {
+    return res.status(400).json({
+      error: true,
+      data: null,
+      message: "Please enter valid information.",
+    });
+  }
 
-//   db.Pizza.create(req.body)
-//     .then((createdPizza) => {
-//       res.json({
-//         error: false,
-//         data: createdPizza,
-//         message: "Successfully created new pizza.",
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({
-//         error: true,
-//         data: null,
-//         message: "Unable to create new pizza.",
-//       });
-//     });
-// });
+  db.Workout.create(req.body)
+    .then((createdWorkout) => {
+      res.json({
+        error: false,
+        data: createdWorkout,
+        message: "Successfully created a new workout.",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "Unable to create new workout.",
+      });
+    });
+});
 
-// router.put("/api/pizzas/:id", (req, res) => {
-//   const ingredientIdToAdd = req.body.ingredientIdToAdd;
-//   db.Pizza.findOneAndUpdate(
-//     { _id: req.params.id },
-//     { $push: { ingredients: arrayOfIngredients } },
-//     { new: true }
-//   )
-//     .then((updatedPizza) => {
-//       console.log(updatedPizza);
-//       res.json({
-//         error: false,
-//         data: updatedPizza,
-//         message: "Successfully updated pizza",
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(500).json({
-//         error: true,
-//         data: null,
-//         message: "unable to update pizza",
-//       });
-//     });
-// });
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: req.body } },
+    { new: true }
+  )
+    .then((updatedWorkout) => {
+      console.log(updatedWorkout);
+      res.json({
+        error: false,
+        data: updatedWorkout,
+        message: "Successfully updated workout",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: true,
+        data: null,
+        message: "unable to update workout",
+      });
+    });
+});
 
-// module.exports = router;
+module.exports = router;
